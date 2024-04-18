2a7ba2e3452687bf4bc23c29fab44191f0122f48
xLaufy <mixol456@gmail.com>
2024-04-18 22:21:56 +0200

    printf still not working as intended

---
+++ FT_PRINTF/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkrawczy <mkrawczy@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 20:21:36 by mkrawczy          #+#    #+#             */
+/*   Updated: 2024/04/17 20:53:53 by mkrawczy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int		ft_printf(const char *form_spec, ...);
+int		ft_putchar(char c);
+int     ft_putstr(char *str);
+int		ft_putnbr(long int n);
+int		ft_putunbr(unsigned long int num);
+int		ft_ptradress(unsigned long int num);
+int		ft_hex(unsigned int num, char format);
+
+#endif
