067fd0d1056a1e09e2fc0551a195d2b95636e42d
Milosz Krawczyk <mkrawczy@student.42warsaw.pl>
2024-05-08 18:10:00 +0200

    finished bonus part of get next line assignment

---
+++ GET_NEXT_LINE/get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkrawczy <mkrawczy@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 16:08:08 by mkrawczy          #+#    #+#             */
+/*   Updated: 2024/05/08 17:52:48 by mkrawczy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# define MAX_FD 2048
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+char	*take_the_line(char *box);
+char	*remains(char *box);
+char	*read_the_file(char *box, int fd);
+
+#endif
