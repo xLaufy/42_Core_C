0edeee7048cd16ab2fea89580f140bc94a93bf4d
Miłosz <93123772+xLaufy@users.noreply.github.com>
2025-02-24 13:17:49 +0100

    Add files via upload

---
+++ PUSH_SWAP/src/handle_errors.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: mkrawczy <mkrawczy@student.42warsaw.pl>    +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
+/*   Created: 2024/06/07 16:10:52 by mkrawczy          #+#    #+#             */
+/*   Updated: 2024/06/07 16:10:52 by mkrawczy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+int	error_syntax(char *str_n) //Define a funtion to handle syntax errors, and returns `1` for `error` should any of the following conditions are met
+{
+	if (!(*str_n == '+'
+			|| *str_n == '-'
+			|| (*str_n >= '0' && *str_n <= '9'))) 
+		return (1);
+	if ((*str_n == '+'
+			|| *str_n == '-')
+		&& !(str_n[1] >= '0' && str_n[1] <= '9')) 
+		return (1);
+	while (*++str_n) 
+	{
+		if (!(*str_n >= '0' && *str_n <= '9')) 
+			return (1);
+	}
+	return (0);
+}
+
+int	error_duplicate(t_stack_node *a, int n) //Define a function that checks for duplicate input numbers in stack `a`
+{
+	if (!a) 
+		return (0);
+	while (a) 
+	{
+		if (a->nbr == n) 
+			return (1);
+		a = a->next; 
+	}
+	return (0);
+}
+
+void	free_stack(t_stack_node **stack) 
+{
+	t_stack_node	*tmp; 
+	t_stack_node	*current;
+
+	if (!stack) 
+		return ;
+	current = *stack;
+	while (current) 
+	{
+		tmp = current->next; 
+		current->nbr = 0; 
+		free(current); 
+		current = tmp; 
+	}
+	*stack = NULL;
+}
+
+void	free_errors(t_stack_node **a) 
+{
+	free_stack(a);
+	ft_printf("Error\n");
+	exit(1);
+}
